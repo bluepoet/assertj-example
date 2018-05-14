98a2b3e67a5807dd1576395aaf165136a25e2bad
poet.me <=poet.me@kakaocorp.com>
2018-05-14 10:08:54 +0900

    [AssertJ Core features highlight]Filtering on a property or a field까지 완료

--- src/test/java/net/bluepoet/assertjex/CoreFeatureHighlightEx.java
+++ src/test/java/net/bluepoet/assertjex/CoreFeatureHighlightEx.java
@@ -4,26 +4,28 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-public class CoreFeatureEx {
+public class CoreFeatureHighlightEx {
     List<Person> persons = new ArrayList<>();
-    Person tester =  new Person("teser", 10);
-    Person bluepoet =  new Person("bluepoet", 30);
-    Person lover =  new Person("lover", 50);
+    Person tester = new Person("tester", 10);
+    Person bluepoet = new Person("bluepoet", 30);
+    Person lover = new Person("lover", 50);
+    Person poet = new Person("poet", 60);
 
     @Before
     public void setUp() throws Exception {
+        tester.addHobby(Hobby.PIANO);
+        bluepoet.addHobby(Hobby.READING);
+        lover.addHobby(Hobby.MOVIE);
+        poet.addHobby(Hobby.PIANO);
+
         persons.add(tester);
         persons.add(bluepoet);
         persons.add(lover);
-
-        persons.get(0).addHobby(Hobby.create("football"));
-        persons.get(2).addHobby(Hobby.create("baseball"));
+        persons.add(poet);
     }
 
     @Test
@@ -42,7 +44,7 @@ public class CoreFeatureEx {
         // given
         // when
         // then
-        assertThat(persons).filteredOn(p -> p.getAge() >= 30).as("나이가 30살 이상인 사람은 몇명일까?").hasSize(2);
+        assertThat(persons).filteredOn(p -> p.getAge() >= 30).as("나이가 30살 이상인 사람은 몇명일까?").hasSize(3);
     }
 
     @Test
@@ -50,7 +52,16 @@ public class CoreFeatureEx {
         // given
         // when
         // then
-        assertThat(persons).filteredOn("hobby", Hobby.create("football")).containsOnly(tester);
+        assertThat(persons).filteredOn("hobby", Hobby.PIANO).containsOnly(tester, poet);
+        assertThat(persons).filteredOn("hobby.name", "reading").containsOnly(bluepoet);
+        assertThat(persons).filteredOn("hobby", notIn(Hobby.PIANO, Hobby.READING)).containsOnly(lover);
+        assertThat(persons).filteredOn("hobby", in(Hobby.MOVIE)).containsOnly(lover);
+        assertThat(persons).filteredOn("hobby", in(Hobby.WALKING)).isEmpty();
+        assertThat(persons).filteredOn("hobby", not(Hobby.PIANO)).containsOnly(bluepoet, lover);
+
+        assertThat(persons).filteredOn("hobby", Hobby.PIANO)
+                .filteredOn("name", not("tester"))
+                .containsOnly(poet);
     }
 
     private class Person {
@@ -80,33 +91,20 @@ public class CoreFeatureEx {
         }
     }
 
-    private static class Hobby {
+    private enum Hobby {
+        PIANO("piano"),
+        WALKING("walking"),
+        READING("reading"),
+        MOVIE("movie");
+
         private String name;
 
-        private Hobby(String name) {
+        Hobby(String name) {
             this.name = name;
-        }
-
-        private static Hobby create(String name) {
-            return new Hobby(name);
         }
 
         public String getName() {
             return name;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Hobby hobby = (Hobby) o;
-            return Objects.equals(name, hobby.name);
-        }
-
-        @Override
-        public int hashCode() {
-
-            return Objects.hash(name);
         }
     }
 }
